8eac59a1b55bcabb74bf8526efeaed4d668f15f2
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-01-25 19:40:47 +0530

    add user module

---
+++ sec/models/user.model.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "brcypt"
+
+const userSchema = new Schema(
+    // username, email, password, name, refreshtoken
+
+    {
+        username:{
+            type: String,
+            unique: true,
+            lowercase: true,
+            require: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            require: true,
+            lowercase: true,
+            unique: true,
+            trim: true
+        },
+        password :{
+            type: String,
+            require: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            require: true,
+            index: true,
+        },
+        refreshToken:{
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.method.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
