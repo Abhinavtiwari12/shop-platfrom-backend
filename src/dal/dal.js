5232a46606b0538da341fdefb415428d4a477e34
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-01-30 12:19:55 +0530

    Add missing exports for findSingleDataAggregate and updateSingleData functions

--- src/dal/dal.js
+++ src/dal/dal.js
@@ -1,3 +1,5 @@
+import { model } from "mongoose"
+
 export const create = async(model,data) => {
     return await model.create(data)
 }
@@ -8,4 +10,12 @@ export const findSingleData = async(model,cond) => {
 
 export const findById = async (model, id) => {
     return await model.findById(id);
+}
+
+export const findSingleDataAggregate = async(model,query) => {
+    return await model.aggregate(query);
+}
+
+export const updateSingleData = async(model,condition,body) => {
+    return await model.findOneAndUpdate(condition,body,{new:true});
 }
