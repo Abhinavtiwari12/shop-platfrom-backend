04f25e88a1f4601ae0956d9d7f83d41cd1a1f084
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-01-30 12:20:56 +0530

    Add searchProducts route to products API for enhanced product searching

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJwtOwner  } from "../middlewares/autho.middlewares.js";
-import { createProduct, updateProduct, deleteProduct } from "../controllers/prodeuct.controller.js";
+import { createProduct, updateProduct, deleteProduct, searchProducts } from "../controllers/prodeuct.controller.js";
 import {uplode} from "../middlewares/multer.middleware.js"
 
 
@@ -11,11 +11,14 @@ const router = Router();
 router.route('/createProduct').post(
     verifyJwtOwner,
     uplode.fields([
-        {name: "productImage",}
+        {name: "productImage",
+            maxCount: 1
+        }
     ]),
 createProduct)
 
 router.route('/updateProduct').post(verifyJwtOwner, updateProduct)
 router.route('/deleteProduct').post(verifyJwtOwner, deleteProduct)
+router.route('/search').get(verifyJwtOwner, searchProducts)
 
 export default router
