38a6be052d51725ab09a910d4d4e8b5f7de68fa5
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-01-30 18:04:47 +0530

    Add mostSearchedproduct function to retrieve top searched products

--- src/dal/dal.js
+++ src/dal/dal.js
@@ -1,4 +1,5 @@
-import { model } from "mongoose"
+import { model } from "mongoose";
+import { Product } from "../models/products.model.js";
 
 export const create = async(model,data) => {
     return await model.create(data)
@@ -19,3 +20,10 @@ export const findSingleDataAggregate = async(model,query) => {
 export const updateSingleData = async(model,condition,body) => {
     return await model.findOneAndUpdate(condition,body,{new:true});
 }
+
+export const mostSearchedproduct = async (limit = 10) => {
+  return await Product
+    .find({ searchCount: { $gt: 0 } })
+    .sort({ searchCount: -1 })
+    .limit(limit);
+};
