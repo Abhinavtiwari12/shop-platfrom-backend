e841361c14afe748f0f43d7394cff14018961838
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-01 23:59:46 +0530

    clean the code

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/autho.middlewares.js";
 import { registerUser, userlogin, userlogout } from "../controllers/user.controller.js";
-
+import { searchProducts } from "../controllers/prodeuct.controller.js";
 
 const router = Router();
 
@@ -11,6 +11,7 @@ const router = Router();
 router.route('/createUser').post(registerUser)
 router.route('/login').post(userlogin)
 router.route('/user-logout').post(verifyJwt, userlogout)
+router.route('/search').get(verifyJwt, searchProducts)
 
 
 export default router
